1633013c87dc1b122adf1a3d5c54f7d6ba473d93
Alumno <ulsa@JesusCorral.local>
2019-08-16 14:34:04 -0700

    Son SizeClasses, los items se adaptan

---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by Alumno on 8/16/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
